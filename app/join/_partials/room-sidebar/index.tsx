0b821654d183d5ef4bfe83075027888e51fbe4cf
aliblackeye <ali.blackeye@hotmail.com>
2024-10-26 12:47:52 +0300

    color

--- app/join/_partials/room-sidebar/index.tsx
+++ app/join/_partials/room-sidebar/index.tsx
@@ -18,7 +18,7 @@ export const RoomSidebar = () => {
     }
   }, [theme]);
   return (
-    <aside className="flex border-r-2">
+    <aside className="flex border-r-2 dark:bg-neutral-900 bg-neutral-100">
       <div className="flex flex-col !w-[60px]">
         <div className="min-h-[63px] gap-2 flex-col border-r bg-neutral-900 h-16 flex items-center justify-end">
           <div className="min-w-10 min-h-10 text-2xl font-bold text-center flex justify-center items-center">
@@ -31,7 +31,7 @@ export const RoomSidebar = () => {
 
         <div
           onClick={() => setTheme(dark ? "light" : "dark")}
-          className="cursor-pointer min-h-[60px] flex items-center justify-center border-r"
+          className="cursor-pointer min-h-[60px] flex items-center justify-center border-r "
         >
           {dark ? <Sun /> : <Moon />}
         </div>
