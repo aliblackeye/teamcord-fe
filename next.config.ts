8ce1be97af32635f343c4fbd5fd5819adaa7f859
aliblackeye <ali.blackeye@hotmail.com>
2024-10-28 01:50:39 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // HTTPS için gerekli yapılandırmalar burada yer almayacak
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
